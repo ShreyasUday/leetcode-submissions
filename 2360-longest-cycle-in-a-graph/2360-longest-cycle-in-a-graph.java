50f886e34761e8bb892669fb8b4577cf30ed744e
Shreyas Uday <143956841+ShreyasUday@users.noreply.github.com>
2026-07-01 02:58:33 +0530

    Time: 50 ms (39.42%), Space: 120.1 MB (29.49%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
@@ -1,9 +1,7 @@
 class Solution {
     public int longestCycle(int[] edges) {
         int n = edges.length;
-        // int node = 0;
         boolean[] visited = new boolean[n];
-        // Arrays.fill(visited,false);
         int res = -1;
         for(int i = 0; i<n; i++){
             if(visited[i] == true) continue;
@@ -24,3 +22,11 @@ class Solution {
         return res;
     }
 }
+
+/*
+    1. first, create a visited array which will tell you which node is processed.
+    2. next, loop from 0 to n (size of edges)
+    3. for every node (iteration) create a map which will store node and time to reach that node.
+    4. run one more loop with conditions like node not -1 and not visited and not in map.
+    5. after this internal loop check if the node is -1 or not if NO, then calculate the current time for the loop from --> currTime( which is 'time' for that iteration ) - time at the current node (from map)
+*/
